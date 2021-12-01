52b8386f8731dbcea04334fb5411ed0ab48f364c
Rom Lubianiker <86227446+RomProject@users.noreply.github.com>
2021-12-01 16:55:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+app.use('/movies',require('./movies'))
+
+
+
+
+app.listen(2323,()=>{console.log("Port 2323")})
